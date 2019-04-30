9b066893ee57705d1a9339a4145d089ae390efd8
lihua <poinseting@163.com>
2019-04-30 15:01:48 +0800

    提交工程

---
+++ error.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>按钮被单击</title>
+<script type="text/javascript">
+<!--
+function BTMsg(msg) { //v1.0
+  alert(msg);
+}
+//-->
+</script>
+</head>
+<body>
+<body background="images/1.jpg"></body>
+  <h1 style="color: red;">sorry... ...！<br />该模块尚在建设中,敬请期待。。。 </h1>
+<body>
+<form id="form1" name="form1" method="post" action="">
+  <label>
+    <!--<input name="button" type="submit" id="button" onclick="BTMsg('铵钮被单击！')" value="按钮" /> -->
+    <input name="button" type="submit" id="button" value="点我返回上一页" onClick="window.open('index.html')" />
+  </label>
+</form>
+
+</body>
+</html>
